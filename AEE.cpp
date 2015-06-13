e2cfa0374cd08dc62b2f9cd30ca5cd082167195d
JingqingZ <zhangjqsmiling@gmail.com>
2015-06-13 17:06:50 +0800

    update

--- AEE.cpp
+++ AEE.cpp
@@ -167,6 +167,9 @@ int AEE::createIndex(const char *entity_file_name) {
 		currentEntity.name = line;
 		currentEntity.length = strlen(line);
 
+		if (currentEntity.length > 39)
+			continue;
+
 		// cut \n
 		if (currentEntity.name[currentEntity.length - 1] == '\n') {
 			currentEntity.name[currentEntity.length - 1] = '\0';
