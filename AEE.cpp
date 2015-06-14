e35a64caf9fb8c51b813d33a20609605784b1007
JingqingZ <zhangjqsmiling@gmail.com>
2015-06-14 14:52:42 +0800

    update

--- AEE.cpp
+++ AEE.cpp
@@ -303,7 +303,7 @@ int AEE::aeeED(const char *document, unsigned threshold, vector<EDExtractResult>
     for (int startpos = 0 ; startpos < endpos; startpos++) {
     	parent = root;
     	currentPos = startpos;
-    	do {
+    	while (currentPos < doclen) {
     		currentChar = charUniMap[(int)document[currentPos]];
     		parent = parent->children[currentChar];
     		currentPos ++;
@@ -372,7 +372,7 @@ int AEE::aeeED(const char *document, unsigned threshold, vector<EDExtractResult>
 					}
 				}
     		}
-    	} while (currentPos <= doclen);
+    	}
     }
 	
 	// sort and unique    
